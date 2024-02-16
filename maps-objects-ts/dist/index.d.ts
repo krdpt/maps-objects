d292934b03abbd8234c11a728a0ddd128ccdbfc1
VolanDeMor1 <volandemort2020@gmail.com>
2024-02-16 16:04:53 +0300

    1.0.8 fixes

--- maps-objects-ts/dist/index.d.ts
+++ maps-objects-ts/dist/index.d.ts
@@ -1,3 +1,97 @@
-export {Stop} from '/index';
-export {Vehicle} from '/index';
-export {VehicleType} from '/index';
+export class Stop {
+    constructor(data: {
+        id: string;
+        index: number;
+        name: string;
+        description: string;
+        lat: number;
+        lon: number;
+        routes: any[];
+        transport: any;
+        kttu: any;
+        trolley: any;
+        tram: any;
+    });
+    id: string;
+    index: number;
+    name: string;
+    description: string;
+    lat: number;
+    lon: number;
+    routes: any[];
+    transport: any;
+    kttu: any;
+    trolley: any;
+    tram: any;
+}
+
+export class Vehicle {
+    constructor(data: {
+        id: string;
+        registration_number: string;
+        model: string;
+        type: string;
+        route: any;
+        route_id: any;
+        lat: number;
+        lng: number;
+        speed: number;
+        arrow: any;
+        kttu: any;
+        long: any;
+        routeless: any;
+    });
+    id: string;
+    registration_number: string;
+    model: string;
+    type: string;
+    route: any;
+    route_id: any;
+    lat: number;
+    lng: number;
+    speed: number;
+    arrow: any;
+    kttu: any;
+    long: any;
+    routeless: any;
+}
+
+export class VehicleInfo {
+    constructor(data: {
+        num: any;
+        model: string;
+        years: any;
+        factory_id: any;
+        built_in: any;
+        exploitation_since: any;
+        depot: any;
+        registration_number: string;
+        comment: any;
+        more_url: any;
+        image_url: any;
+        small_image_url: any;
+        tags: any;
+    });
+    num: any;
+    model: string;
+    years: any;
+    factory_id: any;
+    built_in: any;
+    exploitation_since: any;
+    depot: any;
+    registration_number: string;
+    comment: any;
+    more_url: any;
+    image_url: any;
+    small_image_url: any;
+    tags: any;
+}
+
+export enum VehicleType {
+    TROLLEY = "trolley",
+    BUS = "bus",
+    EBUS = "ebus",
+    TRAM = "tram",
+    SERVICE = "service",
+    COMMERCIAL = "commercial",
+}
