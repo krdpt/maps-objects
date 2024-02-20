6b878c1132d48893ce2b2c5b12fd1b931d96158b
VolanDeMor1 <volandemort2020@gmail.com>
2024-02-20 17:21:13 +0300

    Fix x4

--- maps-objects-kotlin/build.gradle.kts
+++ maps-objects-kotlin/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.9.22"
+    id("maven-publish")
 }
 
 group = "ru.krdpt"
@@ -7,11 +8,24 @@ version = "1.0.4"
 
 repositories {
     mavenCentral()
+    maven ("https://jitpack.io")
 }
 
 dependencies {
     implementation("org.json:json:20231013")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                groupId = "com.github.VolanDeMor1"
+                artifactId = "maps-objects-kotlin"
+                version = project.version as String
+            }
+        }
+    }
 }
 
 tasks.test {
