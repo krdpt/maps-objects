5a0fc48fa7b31935e3d2be00d8255343f581b94b
VolanDeMor1 <volandemort2020@gmail.com>
2024-02-23 23:48:53 +0300

    Improvements

--- maps-objects-kotlin/src/main/kotlin/Vehicle.kt
+++ maps-objects-kotlin/src/main/kotlin/Vehicle.kt
@@ -6,101 +6,58 @@ import org.json.JSONObject
 
 @Serializable
 enum class VehicleType {
-    @SerialName("trolley")
-    TROLLEY,
-
-    @SerialName("bus")
-    BUS,
-
-    @SerialName("ebus")
-    EBUS,
-
-    @SerialName("tram")
-    TRAM,
-
-    @SerialName("service")
-    SERVICE,
-
-    @SerialName("commercial")
-    COMMERCIAL
+    @SerialName("trolley") TROLLEY,
+    @SerialName("bus") BUS,
+    @SerialName("ebus") EBUS,
+    @SerialName("tram") TRAM,
+    @SerialName("service") SERVICE,
+    @SerialName("commercial") COMMERCIAL
 }
 
 @Serializable
-class Vehicle {
-    val id: String
-    val type: VehicleType
+class Vehicle (
+    val id: String,
+    val type: VehicleType,
 
     @SerialName("registration_number")
-    val registrationNumber: String?
-    val model: String?
-    val route: String?
+    val registrationNumber: String?,
+    val model: String?,
+    val route: String?,
+
     @SerialName("route_id")
-    val routeId: String?
+    val routeId: String?,
 
-    val lat: Double
-    val lng: Double
+    val lat: Double,
+    val lng: Double,
 
-    val speed: Double?
-    val arrow: Double?
+    val speed: Double?,
+    val arrow: Double?,
 
-    val kttu: Boolean
-    val long: Boolean
+    val kttu: Boolean,
+    val long: Boolean,
     val routeless: Boolean
+) {
 
-    constructor(
-        id: String,
-        type: VehicleType,
+    constructor(data: JSONObject) : this(
+        data.getString("id"),
+        VehicleType.valueOf(data.getString("type").uppercase()),
 
-        registrationNumber: String?,
-        model: String?,
-        route: String?,
-        routeId: String?,
+        if (data.has("registration_number"))
+            data.getString("registration_number") else null,
+        if (data.has("model")) data.getString("model") else null,
+        if (data.has("route")) data.getString("route") else null,
+        if (data.has("route_id")) data.getString("route_id") else null,
 
-        lat: Double,
-        lng: Double,
+        data.getDouble("lat"),
+        data.getDouble("lng"),
+        if (data.has("speed") && !data.isNull("speed"))
+            data.getDouble("speed") else null,
+        if (data.has("arrow")) data.getDouble("arrow") else null,
 
-        speed: Double?,
-        arrow: Double?,
-
-        kttu: Boolean,
-        long: Boolean,
-        routeless: Boolean
-    ) {
-        this.id = id
-        this.type = type
-        this.registrationNumber = registrationNumber
-        this.model = model
-        this.route = route
-        this.routeId = routeId
-        this.lat = lat
-        this.lng = lng
-        this.speed = speed
-        this.arrow = arrow
-        this.kttu = kttu
-        this.long = long
-        this.routeless = routeless
-    }
-
-    constructor(data: JSONObject) {
-        this.id = data.getString("id")
-        this.type = VehicleType.valueOf(data.getString("type").uppercase())
-
-        this.registrationNumber = if (data.has("registration_number"))
-            data.getString("registration_number") else null
-        this.model = if (data.has("model")) data.getString("model") else null
-        this.route = if (data.has("route")) data.getString("route") else null
-        this.routeId = if (data.has("route_id")) data.getString("route_id") else null
-
-        this.lat = data.getDouble("lat")
-        this.lng = data.getDouble("lng")
-        this.speed = if (data.has("speed") && !data.isNull("speed"))
-            data.getDouble("speed") else null
-        this.arrow = if (data.has("arrow")) data.getDouble("arrow") else null
-
-        this.kttu = data.getBoolean("kttu")
-        this.long = data.getBoolean("long")
-        this.routeless = data.getBoolean("routeless")
-    }
+        data.getBoolean("kttu"),
+        data.getBoolean("long"),
+        data.getBoolean("routeless")
+    )
 
     constructor(json: String): this(JSONObject(json))
 
